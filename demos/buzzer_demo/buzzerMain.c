ac3575a4b76da45a3d4dd8fce43700883d2145b7
juangarciareyes <jagarciarey@miners.utep.edu>
2020-03-27 17:56:43 -0600

    songs :)

--- demos/buzzer_demo/buzzerMain.c
+++ demos/buzzer_demo/buzzerMain.c
@@ -4,9 +4,12 @@
 
 int main() {
     configureClocks();
- 
     buzzer_init();
-    buzzer_set_period(1000);	/* start buzzing!!! */
-
+    switchStatement();
+    //buzzer_set_period(1000);/* start buzzing!!! */
+    //buzzer_set_period(500);
+    //buzzer_set_period(100);
+    //buzzer_set_period(500);
+    //buzzer_set_period(400);
     or_sr(0x18);          // CPU off, GIE on
 }
